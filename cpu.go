79f4751ef29ab2a5d31e029d5d5ba0a8cfb9ea1b
Robert Wertman <robert.wertman@gmail.com>
2013-03-01 14:09:48 -0500

    Started implementation of sparse memory array

--- cpu.go
+++ cpu.go
@@ -8,3 +8,21 @@ var CPU struct {
 		hi, lo, pc, epc, cause, badvaddr int32
 	}
 }
+
+type memory interface {
+	GetByte(int32) byte
+	GetShort(int32) int16
+	GetWord(int32) int32
+	GetDouble(int32) float64
+}
+
+type sparse_memory struct {
+	length int32
+	start list_item
+}
+
+type list_item struct {
+	position int32
+	value int32
+	next *list_item
+}
