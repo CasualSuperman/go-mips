7f5bb3e4a6a2fd18f93bbfe3161e66357945428d
Robert Wertman <robert.wertman@gmail.com>
2013-04-08 13:48:46 -0400

    Added ticks and command execution base.

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -4,6 +4,7 @@ type CPU struct {
 	registers
 	memory struct {
 		hi, lo []byte
+		program []byte
 	}
 	hidden struct {
 		hi, lo, pc, epc, cause, badvaddr int32
@@ -14,9 +15,9 @@ type CPU struct {
 type Syscall func (c *CPU)
 type OperatingSystem map[int32]Syscall
 
-func New(os OperatingSystem) (c CPU) {
-	c.os = os
-	return
+func New(os OperatingSystem) *CPU {
+	c := CPU{os: os}
+	return &c
 }
 
 func (c *CPU) Syscall() {
@@ -26,4 +27,21 @@ func (c *CPU) Syscall() {
 	} else {
 		panic("Syscall not implemented.")
 	}
+}
+
+func (c *CPU) Tick() {
+	memoryOffset = c.hidden.pc >> 2
+	var command int32
+	for i := 0; i < 4; i++ {
+		command = command & c.memory.program[memoryOffset]
+		command = command << 8
+	}
+	pcChanged := c.Execute(command)
+	if !pcChanged {
+		c.hidden.pc += 4
+	}
+}
+
+func (c *CPU) Execute(command int32) bool {
+
 }
